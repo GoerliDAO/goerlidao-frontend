aae833e017190e975aa1d74e1a0ac311e970cc4d
kanema <kanema20@gmail.com>
2023-04-21 21:21:10 -0400

    dafsdfa

--- src/hooks/wagmi.ts
+++ src/hooks/wagmi.ts
@@ -77,7 +77,7 @@ export const { chains, provider, webSocketProvider } = configureChains(
     },
     { ...chain.goerli, rpcUrls: { default: "https://rpc.ankr.com/eth_goerli" } },
     { ...chain.sepolia, rpcUrls: { default: "https://rpc.ankr.com/eth_sepolia" } },
-    { ...chain.foundry, rpcUrls: { default: "https://127.0.0.1:8545" } },
+    // { ...chain.foundry, rpcUrls: { default: "https://127.0.0.1:8545" } },
   ],
   [
     jsonRpcProvider({ rpc: chain => ({ http: chain.rpcUrls.default }) }),
