b9ece5a0c19fc1b032703b2e51adbc28880c1f73
Tyler Brangman <brangman.tyler@gmail.com>
2023-07-17 12:37:56 -0300

    touch up footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,18 +16,18 @@ const Footer = () => {
     >
       <div className="container mx-auto flex items-end justify-between font-semibold">
         <div className="grid grid-cols-1 md:grid-cols-1 grid-rows-3 gap-2 text-xs">
-          <a href="">Donate</a>
-          <a href="">Docs</a>
-          <a href="">Stats</a>
+          <a target="_blank" href="https://twitter.com/GoerliDAO">
+            Twitter
+          </a>
         </div>
         <div className="flex flex-col justify-center items-center">
           {theme === "dark" ? <FooterApeWhite className="w-20" /> : <FooterApeBlue className="w-20" />}
           <span className="text-xs">© Gööööööööööööööööööööööööerli</span>
         </div>
         <div className="grid grid-cols-1 md:grid-rows-3 gap-2 text-xs">
-          <a href="">Twitter</a>
-          <a href="">Telegram</a>
-          <a href="">Discord</a>
+          <a target="_blank" href="https://t.me/goerli_dao">
+            Telegram
+          </a>
         </div>
       </div>
     </div>
